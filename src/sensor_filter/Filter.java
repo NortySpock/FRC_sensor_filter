4e2e342ee504dba1bbdb3f18bfc21bf8944a2403
David Norton <david.k.norton@gmail.com>
2017-12-30 12:30:42 -0600

    Added comments and a README, slightly cleaned up code.

--- src/sensor_filter/Filter.java
+++ src/sensor_filter/Filter.java
@@ -4,55 +4,92 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Filter {
-    private static Random random = new Random();
+    private static Random random = new Random(); // lets us make random numbers
+
+    private static final int buffer_size = 10;
+
+    // private variables for the naive rolling average
     private static int naive_counter;
-    private double naieve_reading[] = new double[10];
+    private double naive_reading[] = new double[buffer_size];
     private boolean naive_avg_10_ready = false;
+
+    // private variables for the filtered rolling average
     private boolean smarter_avg_ready = false;
     private static int smarter_counter;
-    private double smarter_sorted_reading[] = new double[10];
-    private double smarter_current_reading[] = new double[10];
-    private int smarter_spacer = 2;
 
+    private double smarter_sorted_reading[] = new double[buffer_size];
+    private double smarter_current_reading[] = new double[buffer_size];
+    private static final int smarter_spacer = 2; // this indicates the number of
+                                                 // values on
+    // each end to drop from the average
+
+    // generates a random "reading from the sensor". Assume this is a distance
+    // ultrasonic sensor that is 10 inches from the target.
     public static double generate_reading() {
+        // store a collection of possible distance readings. Notice that that
+        // the values generally range from 9.0 to 11.0
+        // The actual average is 10.0, with outliers of 0 and 20.
+        // So we expect to see averages around 10,
+        // and a rolling average reading of less than 9 or greater than 11
+        // indicates our average is being heavily skewed by an outlier.
         double possible_readings[] = { 0.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7,
                 9.8, 9.9, 10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8,
                 10.9, 20.0 };
+        // spits out a random reading from the possible readings above
         return possible_readings[random.nextInt(20)];
     }
 
-    public double naive_avg_10(double input) {
+    public double naive_avg(double input) {
+        // load a value into the buffer
+        naive_reading[naive_counter] = input;
         naive_counter = naive_counter + 1;
-        if (naive_counter >= naieve_reading.length) {
+
+        // First we must fill the buffer and mark it as ready.
+        // Once the buffer is full, we're ready to return valid values.
+        if (naive_counter >= naive_reading.length) {
             naive_counter = 0;
             naive_avg_10_ready = true;
         }
-        naieve_reading[naive_counter] = input;
+
+        // Return an invalid distance if we're not ready yet.
         if (!naive_avg_10_ready) {
             return -1.0;
         } else {
+            // get the average value in the buffer.
             double sum = 0.0;
-            for (int i = 0; i < naieve_reading.length; i = i + 1) {
-                sum = sum + naieve_reading[i];
+            for (int i = 0; i < naive_reading.length; i = i + 1) {
+                sum = sum + naive_reading[i];
             }
-            return sum / naieve_reading.length;
+            return sum / naive_reading.length;
         }
 
     }
 
     public double smarter_avg(double input) {
+        // load a value into the buffer
+        smarter_current_reading[smarter_counter] = input;
         smarter_counter++;
+
+        // First we must fill the buffer and mark it as ready.
+        // Once the buffer is full, we're ready to return valid values.
         if (smarter_counter >= smarter_current_reading.length) {
             smarter_counter = 0;
             smarter_avg_ready = true;
         }
-        smarter_current_reading[smarter_counter] = input;
+
+        // Return an invalid distance if we're not ready yet.
         if (!smarter_avg_ready) {
             return -1.0;
         } else {
+            // Sort the buffer with an ordering
             smarter_sorted_reading = smarter_current_reading;
             Arrays.sort(smarter_sorted_reading);
+
             double sum = 0.0;
+            // here, we're dropping the lowest and highest n values (where n is
+            // the value of smarter_spacer) from the average. This will
+            // generally leave outliers out of the average, leading to a more
+            // stable average
             for (int i = smarter_spacer; i < smarter_sorted_reading.length
                     - smarter_spacer; i++) {
                 sum += smarter_sorted_reading[i];
--- src/sensor_filter/Main.java
+++ src/sensor_filter/Main.java
@@ -3,9 +3,20 @@ package sensor_filter;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Simple average filter:");
         Filter f = new Filter();
         int counter = 0;
-        while (counter < 50) {
+        while (counter < 30) {
+            counter++;
+            System.out.println(Double.toString(f.naive_avg(Filter.generate_reading())));
+        }
+
+        System.out.println(" ");
+
+        System.out.println("Smarter filter:");
+        f = new Filter();
+        counter = 0;
+        while (counter < 30) {
             counter++;
             System.out.println(f.smarter_avg(Filter.generate_reading()));
         }
